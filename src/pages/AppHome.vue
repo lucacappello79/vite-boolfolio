ace815f2d7fda456cca359190a55ea84f973cece
lucacappello79 <postt79@gmail.com>
2023-05-30 17:36:33 +0200

    feat: styling home

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -4,12 +4,33 @@ export default {
 }
 </script>
 
-<template>
+<!-- <template>
     <div class="container py-5">
         <h1>Homepage test</h1>
 
         <hr>
         <router-link :to="{ name: 'aboutUsPage' }">About us</router-link>
+    </div>
+</template> -->
+
+<template>
+    <div class="container py-5">
+        <div class="jumbo text-center">
+            <h1 class="display-4">Welcome to My Portfolio</h1>
+            <hr class="my-4">
+            <p>Learn more about me, check my projects or contact me for more information.</p>
+            <div class="d-flex justify-content-center">
+                <div class="mr-3">
+                    <router-link class="btn btn-primary btn-lg" :to="{ name: 'aboutUsPage' }">Learn more about
+                        me</router-link>
+                </div>
+                <div class="ml-3">
+                    <router-link class="btn btn-success btn-lg" :to="{ name: 'projectsIndexPage' }"
+                        style="margin-left: 30px;">View my
+                        projects</router-link>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
