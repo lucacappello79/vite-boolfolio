6dc691b56bf2fb7394a28a8d7323725e42e98599
lucacappello79 <postt79@gmail.com>
2023-05-30 17:44:20 +0200

    feat: styling about me page

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -6,8 +6,35 @@ export default {
 }
 </script>
 
-<template>
+<!-- <template>
     <div class="container py-5">
         About us
     </div>
-</template>
+</template> -->
+
+<template>
+    <div class="container py-5">
+        <div class="jumbo text-center">
+            <h1 class="display-4">About Me</h1>
+            <p class="lead">I am a junior web developer with a strong desire to learn and grow.</p>
+            <hr class="my-4">
+
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>My Mission</h2>
+                    <p>As a junior web developer, my mission is to absorb as much knowledge as possible and develop high
+                        quality software solutions. Despite having limited experience, I am committed to learning and
+                        improving my skills on a daily basis. I believe that with dedication and hard work, I can contribute
+                        significantly to any project.</p>
+                </div>
+
+                <div class="col-md-6">
+                    <h2>My Vision</h2>
+                    <p>My vision is to become a trusted and competent professional in the IT industry, able to deliver
+                        innovative solutions to complex problems. I see each new project as an opportunity to learn and grow
+                        professionally. I am excited about the journey ahead and I look forward to the challenges and
+                        accomplishments that lie ahead.</p>
+            </div>
+        </div>
+    </div>
+</div></template>
