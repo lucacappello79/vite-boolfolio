8f1caf3017dd2dc4eb47611c76d8daeb94d70f41
lucacappello79 <postt79@gmail.com>
2023-05-30 17:58:44 +0200

    feat: styling about

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -12,7 +12,7 @@ export default {
     </div>
 </template> -->
 
-<template>
+<!-- <template>
     <div class="container py-5">
         <div class="jumbo text-center">
             <h1 class="display-4">About Me</h1>
@@ -37,4 +37,38 @@ export default {
             </div>
         </div>
     </div>
-</div></template>
+</div>
+</template> -->
+
+<template>
+    <div class="container py-5">
+        <div class="jumbotron text-center">
+            <h1 class="display-4">About Me</h1>
+            <!-- <img src="/public/luca.jpg" class="img-fluid rounded-circle mx-auto d-block mt-4" alt="Your name">-->
+            <img src="/public/luca.jpg" style="width: 200px; height: 200px;" class="mx-auto mb-4 d-block mt-4"
+                alt="Your name">
+
+
+            <p class="lead">I am a junior web developer with a strong desire to learn and grow.</p>
+            <hr class="my-4">
+
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>My Mission</h2>
+                    <p>As a junior web developer, my mission is to absorb as much knowledge as possible and develop high
+                        quality software solutions. Despite having limited experience, I am committed to learning and
+                        improving my skills on a daily basis. I believe that with dedication and hard work, I can contribute
+                        significantly to any project.</p>
+                </div>
+
+                <div class="col-md-6">
+                    <h2>My Vision</h2>
+                    <p>My vision is to become a trusted and competent professional in the IT industry, able to deliver
+                        innovative solutions to complex problems. I see each new project as an opportunity to learn and grow
+                        professionally. I am excited about the journey ahead and I look forward to the challenges and
+                        accomplishments that lie ahead.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
