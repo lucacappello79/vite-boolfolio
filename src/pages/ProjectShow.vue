c9accbc4fe07474a91c4cd30147bc281050471a6
lucacappello79 <postt79@gmail.com>
2023-06-04 17:41:53 +0200

    feat: updated single porject page aka projectshow

--- src/pages/ProjectShow.vue
+++ src/pages/ProjectShow.vue
@@ -1,5 +1,9 @@
 <script>
+
+import axios from 'axios';
+
 export default {
+
     name: 'ProjectShow',
 
     data() {
@@ -7,7 +11,12 @@ export default {
 
             project: {},
 
+            isLoading: true,
+            projectFound: false,
+
             projectSlug: '',
+
+            baseUrl: 'http://127.0.0.1:8000/',
 
         }
     },
@@ -15,7 +24,17 @@ export default {
     mounted() {
 
         // console.log(this.$route);
-        this.projectSlug = this.$route.params.slug
+        this.projectSlug = this.$route.params.slug;
+        this.getProject();
+
+    },
+
+    computed: {
+
+        projectImage() {
+
+            return this.baseUrl + 'storage/' + this.project.cover_image;
+        }
 
     },
 
@@ -23,10 +42,23 @@ export default {
 
         getProject() {
 
-            axios.get('http://127.0.0.1:8000/api/projects/' + this.projectSlug).then(response => {
+            axios.get(this.baseUrl + 'api/projects/' + this.projectSlug).then(response => {
 
-                this.project = response.data.project;
-            })
+                console.log(response.data);
+                this.isLoading = false;
+
+                if (response.data.success) {
+
+                    this.project = response.data.project;
+                    this.projectFound = true;
+
+                } else {
+
+                    this.projectFound = false;
+
+                }
+
+            });
         }
     },
 
@@ -34,7 +66,54 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1 class="container py-5">{{ project.title }}</h1>
+    <div v-if="isLoading" id="spinner-container">
+
+        <div class="spinner-border" role="status">
+            <span class="visually-hidder">Loading...</span>
+        </div>
+
+    </div>
+
+
+    <div v-else class="container py-5">
+        <div v-if="projectFound">
+            <div class="project-image text-centered">
+                <img :src="projectImage" alt="fnf">
+            </div>
+            <h1>{{ project.title }}</h1>
+            <hr>
+            <p>{{ project.content }}</p>
+        </div>
+        <div v-else>
+            <div class="alert alert-danger" role="alert">
+                No project found
+            </div>
+        </div>
+
     </div>
 </template>
+
+
+<style lang="scss">
+#spinner-container {
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 600px;
+
+    .project-image img {
+
+        max-height: 400px;
+        object-fit: cover;
+
+    }
+
+}
+</style>
